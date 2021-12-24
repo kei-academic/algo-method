ea2f95aebe4f59b28ff49ac92455c2f56c1b08a8
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2021-12-24 17:14:52 +0900

    :tada: `Create` algo-method
    リポジトリの作成．

---
+++ 全探索(beta)/配列の全探索/1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+using vl = vector<long>;
+using vvl = vector<vl>;
+typedef long long ll;
+const double PI = 3.1415926535897932384626;
+//-----------------------------------------------------------------
+ll n, v;
+
+int main(void) {
+    cin >> n >> v;
+    vl arr(n);
+    rep(i, n) {
+        cin >> arr.at(i);
+    }
+
+    bool frag = false;
+    rep(i, n) {
+        if (arr.at(i) == v) {
+            frag = true;
+        }
+    }
+
+    cout << (frag ? "Yes" : "No") << endl;
+
+    return 0;
+}
