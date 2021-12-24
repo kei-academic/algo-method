ea48e8aeba459d144f6cc8cd22c128d2b579f728
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2021-12-24 22:36:04 +0900

    :+1: `Add` 4.cpp
    配列の全検索の4.cppを追加．

---
+++ 全探索(beta)/配列の全探索/4.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+using vl = vector<long>;
+using vvl = vector<vl>;
+typedef long long ll;
+const double PI = 3.1415926535897932384626;
+//-----------------------------------------------------------------
+ll n, v;
+
+int main(void) {
+    cin >> n >> v;
+    vl arr(n);
+    rep(i, n) {
+        cin >> arr.at(i);
+    }
+
+    ll index = -1;
+    rep(i, n) {
+        if (arr.at(i) == v) {
+            index = i;
+        }
+    }
+
+    cout << index << endl;
+
+    return 0;
+}
