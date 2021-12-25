c4e60615a78736583b8cc612849b334cba0e992d
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2021-12-25 12:04:23 +0900

    :+1: `Add` 2.cpp
    文字列の全検索の2.cppを追加．

---
+++ 全探索(beta)/文字列の全探索/2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+using vl = vector<long>;
+using vvl = vector<vl>;
+typedef long long ll;
+const double PI = 3.1415926535897932384626;
+//-----------------------------------------------------------------
+string s;
+
+int main(void) {
+    cin >> s;
+
+    bool flag = true;
+    for (int i = 0; i < s.size(); i++) {
+        if (s.at(i) != s.at(s.size()-i-1)) {
+            flag = not flag;
+            break;
+        }
+    }
+
+    cout << (flag ? "Yes" : "No") << endl;
+
+    return 0;
+}
