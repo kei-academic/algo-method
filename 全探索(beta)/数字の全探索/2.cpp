47562cdbbde578d090e8701c1df0b4600a5cba9b
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2021-12-25 11:04:15 +0900

    :+1: `Add` 2.cpp
    数字の全検索の2.cppを追加．

---
+++ 全探索(beta)/数字の全探索/2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+using vl = vector<long>;
+using vvl = vector<vl>;
+typedef long long ll;
+const double PI = 3.1415926535897932384626;
+//-----------------------------------------------------------------
+ll n;
+
+int main(void) {
+    cin >> n;
+
+    ll count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n%i == 0) {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
