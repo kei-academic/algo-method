3e6a6de8b2c239e68e7e75176a7ccca243044d19
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2021-12-27 06:42:05 +0900

    :+1: `Add` sim2.cpp

---
+++ 論理的思考力を鍛える！文章題(beta)/基本的な算数・前編/sim2.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* accelration */
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+struct Fast {Fast() {std::cin.tie(0); ios::sync_with_stdio(false);}} fast;
+
+/* alias */
+using ull  = unsigned long long;
+using ll   = long long;
+using vi   = vector<int>;
+using vl   = vector<long>;
+using vll  = vector<long long>;
+using vvi  = vector<vi>;
+using vvl  = vector<vl>;
+using vvll = vector<vll>;
+using vs   = vector<string>;
+using pll  = pair<ll, ll>;
+
+/* define short */
+#define pb          push_back
+#define mp          make_pair
+#define fi          first
+#define se          second
+#define all(x)      (x).begin(), (x).end()
+#define YESNO(bool) cout << (bool ? "YES" : "NO") << endl;
+#define YesNo(bool) cout << (bool ? "Yes" : "No") << endl;
+#define yesno(bool) cout << (bool ? "yes" : "no") << endl;
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = a; i < (ll)(n); i++)
+#define rep(i, n)     reps(i, 0, n)
+#define rrep(i, n)    reps(i, 1, n + 1)
+#define repd(i, n)    for (ll i = n-1; i >= 0; i--)
+#define rrepd(i, n)   for (ll i = n; i >= 1; i--)
+
+/* const */
+const double PI = 3.1415926535897932384626;
+//-----------------------------------------------------------------
+int main(void) {
+    cout << 1000 - 110 * 8 << endl;
+
+    return 0;
+}
