466398ee18bfa3e9385b10db94dc23301de1d65b
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2021-12-31 00:19:27 +0900

    :sparkles: `Add` 1.cpp
    分配算1(プログラミング)を追加．

---
+++ 論理的思考力を鍛える！文章題(beta)/分配算1(プログラミング)/1.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* accelration */
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+struct Fast {Fast() {std::cin.tie(0); ios::sync_with_stdio(false);}} fast;
+
+/* alias */
+using ull  = unsigned long long;
+using ll   = long long;
+using vi   = vector<int>;
+using vl   = vector<long>;
+using vll  = vector<long long>;
+using vvi  = vector<vi>;
+using vvl  = vector<vl>;
+using vvll = vector<vll>;
+using vs   = vector<string>;
+using pll  = pair<ll, ll>;
+
+/* define short */
+#define pb          emplace_back
+#define mp          make_pair
+#define fi          first
+#define se          second
+#define endl        "\n"
+#define all(x)      (x).begin(), (x).end()
+#define YESNO(bool) cout << (bool ? "YES" : "NO") << endl;
+#define YesNo(bool) cout << (bool ? "Yes" : "No") << endl;
+#define yesno(bool) cout << (bool ? "yes" : "no") << endl;
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = a; i < (ll)(n); i++)
+#define rep(i, n)     reps(i, 0, n)
+#define rrep(i, n)    reps(i, 1, n + 1)
+#define repd(i, n)    for (ll i = n-1; i >= 0; i--)
+#define rrepd(i, n)   for (ll i = n; i >= 1; i--)
+
+/* constexpr */
+constexpr double PI = 3.1415926535897932384626;
+template<typename T>
+constexpr double DigToRad(T x) { return x * PI / 180; }
+//-----------------------------------------------------------------
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll a;
+    cin >> a;
+
+    cout << a / 3 * 2 << endl;
+
+    return 0;
+}
