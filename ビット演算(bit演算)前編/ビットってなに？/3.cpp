b4875d96187a5189e70e9778625a1f412168f9d2
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2021-12-31 22:29:40 +0900

    :+1: `Add` 3.cpp

---
+++ ビット演算(bit演算)前編/ビットってなに？/3.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* accelration */
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+/* alias */
+using ull  = unsigned long long;
+using ll   = long long;
+using vi   = vector<int>;
+using vl   = vector<long>;
+using vll  = vector<long long>;
+using vvi  = vector<vi>;
+using vvl  = vector<vl>;
+using vvll = vector<vll>;
+using vs   = vector<string>;
+using pll  = pair<ll, ll>;
+
+/* define short */
+#define pb          emplace_back
+#define mp          make_pair
+#define fi          first
+#define se          second
+#define endl        '\n'
+#define all(x)      (x).begin(), (x).end()
+#define each(y, x)  for (auto y : x)
+#define YESNO(bool) cout << (bool ? "YES" : "NO") << endl;
+#define YesNo(bool) cout << (bool ? "Yes" : "No") << endl;
+#define yesno(bool) cout << (bool ? "yes" : "no") << endl;
+
+/* REP macro */
+#define reps(i, a, n) for (ll i = a; i < (ll)(n); i++)
+#define rep(i, n)     reps(i, 0, n)
+#define rrep(i, n)    reps(i, 1, n + 1)
+#define repd(i, n)    for (ll i = n-1; i >= 0; i--)
+#define rrepd(i, n)   for (ll i = n; i >= 1; i--)
+
+/* constexpr */
+constexpr double PI = 3.1415926535897932384626;
+template<typename T>
+constexpr double DigToRad(T x) { return x * PI / 180; }
+//-----------------------------------------------------------------
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s = "0011010010";
+    rep(i, s.size()) {
+        if (s.at(i) == '1') {
+            cout << s.size() - i - 1 << endl;
+
+            return 0;
+        }
+    }
+}
